37120cea7aa5639eead8793fe4916179bdcb42ee
Andrei Toledo <andreiltoledo@hotmail.com>
2017-08-09 17:23:54 -0300

    primeiro commit

---
+++ src/main/java/com/andreitoledo/erp/model/TipoEmpresa.java
@@ -0,0 +1,20 @@
+package com.andreitoledo.erp.model;
+
+public enum TipoEmpresa {
+	
+	MEI("Microempreendedor Individual"), 
+	EIRELI("Empresa Individual de Responsabilidade Limitada"),
+	LTDA("Sociedade Limitada"),
+	SA("Sociedade Anônima");
+	
+	private String descricao;
+
+	TipoEmpresa(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+}
