ed650b3b761c7272850b316c04a42a88a64f2c70
Andrei Toledo <andreiltoledo@hotmail.com>
2017-09-11 02:47:10 -0300

    Selecionando linhas da tabela de dados

--- src/main/java/com/andreitoledo/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/andreitoledo/erp/controller/GestaoEmpresasBean.java
@@ -112,4 +112,12 @@ public class GestaoEmpresasBean implements Serializable {
 	public Empresa getEmpresa() {
 		return empresa;
 	}
+	
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+	
+	public boolean isEmpresaSeleciona() {
+        return empresa != null && empresa.getId() != null;
+    }
 }
